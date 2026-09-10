660ed58e6d4ebd623dbe88f732041c25ac03d6d5
cockcut <105777311+cockcut@users.noreply.github.com>
2026-09-10 13:21:20 +0900

    Update radius_install.sh

--- radius_install.sh
+++ radius_install.sh
@@ -214,11 +214,11 @@ BEGIN {
 # --- 6. daloRADIUS 설정 ---
 echo "--- 6. daloRADIUS 설정 중..."
 sudo cp "${WEB_ROOT}/radius/app/common/includes/daloradius.conf.php.sample" "${WEB_ROOT}/radius/app/common/includes/daloradius.conf.php"
-####sed -i "s/\$configValues\['CONFIG_DB_ENGINE'\] = '.*';/\$configValues\['CONFIG_DB_ENGINE'\] = 'mysqli';/" "${WEB_ROOT}/radius/library/daloradius.conf.php"
-sudo sed -i "s/\$configValues\['CONFIG_DB_HOST'\] = '.*';/\$configValues\['CONFIG_DB_HOST'\] = '${MYSQL_HOST}';/" "${WEB_ROOT}/radius/library/daloradius.conf.php"
-sudo sed -i "s/\$configValues\['CONFIG_DB_USER'\] = '.*';/\$configValues\['CONFIG_DB_USER'\] = '${MYSQL_USER}';/" "${WEB_ROOT}/radius/library/daloradius.conf.php"
-sudo sed -i "s/\$configValues\['CONFIG_DB_PASS'\] = '.*';/\$configValues\['CONFIG_DB_PASS'\] = '${MYSQL_PASSWORD}';/" "${WEB_ROOT}/radius/library/daloradius.conf.php"
-sudo sed -i "s/\$configValues\['CONFIG_DB_NAME'\] = '.*';/\$configValues\['CONFIG_DB_NAME'\] = '${MYSQL_DATABASE}';/" "${WEB_ROOT}/radius/library/daloradius.conf.php"
+####sed -i "s/\$configValues\['CONFIG_DB_ENGINE'\] = '.*';/\$configValues\['CONFIG_DB_ENGINE'\] = 'mysqli';/" "${WEB_ROOT}/radius/app/common/includes/daloradius.conf.php"
+sudo sed -i "s/\$configValues\['CONFIG_DB_HOST'\] = '.*';/\$configValues\['CONFIG_DB_HOST'\] = '${MYSQL_HOST}';/" "${WEB_ROOT}/radius/app/common/includes/daloradius.conf.php"
+sudo sed -i "s/\$configValues\['CONFIG_DB_USER'\] = '.*';/\$configValues\['CONFIG_DB_USER'\] = '${MYSQL_USER}';/" "${WEB_ROOT}/radius/app/common/includes/daloradius.conf.php"
+sudo sed -i "s/\$configValues\['CONFIG_DB_PASS'\] = '.*';/\$configValues\['CONFIG_DB_PASS'\] = '${MYSQL_PASSWORD}';/" "${WEB_ROOT}/radius/app/common/includes/daloradius.conf.php"
+sudo sed -i "s/\$configValues\['CONFIG_DB_NAME'\] = '.*';/\$configValues\['CONFIG_DB_NAME'\] = '${MYSQL_DATABASE}';/" "${WEB_ROOT}/radius/app/common/includes/daloradius.conf.php"
 sudo chown -R apache:apache "${WEB_ROOT}/radius"
 sudo chmod -R 775 "${WEB_ROOT}/radius"
 
